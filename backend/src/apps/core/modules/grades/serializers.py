75562d4d0cfa22f546fa6c7a5b85d9cd3ff9f3b8
Limkaa <khamraevalim@gmail.com>
2023-03-19 18:20:04 +0600

    Grades serializer fix

--- backend/src/apps/core/modules/grades/serializers.py
+++ backend/src/apps/core/modules/grades/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Grade
 
@@ -7,14 +7,19 @@ from .models import Grade
 class GradeSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     school_id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        required=True,
-        validators=[UniqueValidator(queryset=Grade.objects.all())]
-    )
+    name = serializers.CharField(required=True)
     is_active = serializers.BooleanField(required=True)
     updated_at = serializers.DateTimeField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     
+    class Meta:
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Grade.objects.all(),
+                fields=['school_id', 'name']
+            )
+        ]
+        
     def create(self, validated_data):
         return Grade.objects.create(**validated_data)
     

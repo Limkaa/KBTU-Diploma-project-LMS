4edce3534faa00a2541471e78c3ef2f05db45a1b
Limkaa <khamraevalim@gmail.com>
2023-04-01 15:51:43 +0600

    Changed to grade serializers and fixed views

--- backend/src/apps/core/modules/grades/views.py
+++ backend/src/apps/core/modules/grades/views.py
@@ -1,23 +1,24 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework import generics
 
 from .models import Grade
+from ..schools.models import School
 from . import serializers
 
 from .. import permissions
 
 
-class GradeListCreateAPI(generics.ListCreateAPIView):
+class GradeCreateAPI(generics.CreateAPIView):
     permission_classes = [
         permissions.IsManager,
-        permissions.OnlyOwnSchool
+        permissions.OnlyOwnSchoolObject
     ]
-    serializer_class = serializers.GradeSerializer
-    
-    def get_queryset(self):
-        return Grade.objects.filter(school=self.kwargs["school_id"])
+    serializer_class = serializers.GradeModelSerializer
             
     def perform_create(self, serializer):
-        serializer.save(school_id=self.kwargs["school_id"])
+        school = serializer.validated_data.get('school')
+        self.check_object_permissions(self.request, school)
+        return super().perform_create(serializer)
 
 
 class GradeRetrieveUpdateAPI(generics.RetrieveUpdateAPIView):
@@ -25,5 +26,24 @@ class GradeRetrieveUpdateAPI(generics.RetrieveUpdateAPIView):
         permissions.IsManager,
         permissions.OnlyOwnSchoolObject
     ]
-    serializer_class = serializers.GradeSerializer
+    serializer_class = serializers.GradeModelSerializer
     queryset = Grade.objects.all()
+    
+    def perform_update(self, serializer):
+        school = serializer.validated_data.get('school')
+        self.check_object_permissions(self.request, school)
+        return super().perform_update(serializer)
+
+
+class SchoolGradesListAPI(generics.ListAPIView):
+    permission_classes = [
+        permissions.OnlyOwnSchoolObject,
+        permissions.IsManager
+    ]
+    serializer_class = serializers.GradeModelSerializer
+    
+    def get_queryset(self):
+        school = get_object_or_404(School, pk = self.kwargs['school_id'])
+        self.check_object_permissions(self.request, school)
+        return Grade.objects.filter(school = school)
+  

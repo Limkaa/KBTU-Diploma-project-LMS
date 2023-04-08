a497b22933076cbd8a8221cd9b60404905dc33ae
Limkaa <khamraevalim@gmail.com>
2023-04-08 18:19:14 +0600

    Assignments functionality added

---
+++ backend/src/apps/core/modules/assignments/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from .models import Assignment
+
+from ..terms.serializers import TermModelSerializer
+
+
+class AssignmentModelSerializer(serializers.ModelSerializer):  
+    class Meta:
+        model = Assignment
+        fields = "__all__"
+        read_only_fields = ['course', 'term', 'created_at', 'updated_at']
+
+
+class AssignmentModelNestedSerializer(AssignmentModelSerializer):
+    term = TermModelSerializer(read_only=True)
+
+
+class UpdateAssignmentSerializer(AssignmentModelSerializer):
+    def to_representation(self, instance):
+        serializer = AssignmentModelNestedSerializer(instance)
+        return serializer.data
+
+
+class CreateAssignmentSerializer(AssignmentModelSerializer):
+    class Meta(AssignmentModelSerializer.Meta):
+        read_only_fields = ['course', 'created_at', 'updated_at']
+        
+    def to_representation(self, instance):
+        serializer = AssignmentModelNestedSerializer(instance)
+        return serializer.data
+
+
+__all__ = [
+    "AssignmentModelSerializer",
+    "AssignmentModelNestedSerializer",
+    "UpdateAssignmentSerializer",
+    "CreateAssignmentSerializer"
+]

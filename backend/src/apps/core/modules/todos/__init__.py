ad65052156575fc3a3b58c0800253a7b09003722
Limkaa <khamraevalim@gmail.com>
2023-04-29 20:18:42 +0600

    Todos module developed

---
+++ backend/src/apps/core/modules/todos/__init__.py
@@ -0,0 +1,4 @@
+from .apps import TodosConfig
+
+module_path = TodosConfig.name
+urls_path = module_path + '.apis'
--- backend/src/apps/core/modules/todos/admin.py
+++ backend/src/apps/core/modules/todos/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from ...utils.admin import CustomModelAdmin
 
-# Register your models here.
+from .models import Todo
+
+
+@admin.register(Todo)
+class TodoAdmin(CustomModelAdmin):
+    model = Todo
+    
+    raw_id_fields = (
+        "user",
+    )
+    list_display = (
+        "user",
+        "name",
+        "is_done",
+        "priority"
+    )
+    search_fields = (
+        "user__first_name",
+        "user__last_name",
+        "user__email",
+        "name",
+    )
+
+    list_filter = (
+        "is_done",
+        "priority"
+    )
+
+    fieldsets = (
+        (
+            "Todo information",
+            {
+                "fields": (
+                    "user",
+                    "name",
+                    "description",
+                    "is_done",
+                    "priority"
+                )
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at"
+                )
+            },
+        )
+    )
+    
+    base_read_only_fields = ["created_at", "updated_at"]

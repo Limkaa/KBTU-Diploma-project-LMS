06ef6988106e6a9084c1b44b4701a5a1a53136a0
Limkaa <khamraevalim@gmail.com>
2023-05-01 17:40:29 +0600

    Enrollments module connected to core apis urls

--- backend/src/apps/core/apis/urls.py
+++ backend/src/apps/core/apis/urls.py
@@ -6,6 +6,7 @@ modules = [
     APIConfig(awards.urls_path, active=True),
     APIConfig(todos.urls_path, active=True),
     APIConfig(communities.urls_path, active=True),
+    APIConfig(enrollments.urls_path, active=True),
 ]
 
 urlpatterns = [module.connect() for module in modules]
---
+++ backend/src/apps/core/modules/enrollments/__init__.py
@@ -0,0 +1,4 @@
+from .apps import EnrollmentsConfig
+
+module_path = EnrollmentsConfig.name
+urls_path = module_path + '.apis'

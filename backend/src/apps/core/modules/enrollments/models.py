b705c97236403dd9b37c9b2eda8eb03130330e84
Limkaa <khamraevalim@gmail.com>
2023-05-02 18:54:43 +0600

    Updated validation in Enrollment model

--- backend/src/apps/core/modules/enrollments/models.py
+++ backend/src/apps/core/modules/enrollments/models.py
@@ -44,6 +44,25 @@ class Enrollment(CustomModel):
     updated_at = models.DateTimeField(auto_now=True)
     
     non_updatable_fields = ['id', 'student', 'subject', 'year', 'created_at']
+    
+    related_fields = [
+        'student',
+        'student__group',
+        'student__group__grade',
+        'student__group__teacher',
+        'student__user',
+        'student__user__school',
+        'subject',
+        'subject__grade',
+        'year',
+        'course',
+        'course__subject',
+        'course__subject__grade',
+        'course__year',
+        'course__group',
+        'course__group__grade',
+        'course__group__teacher',
+    ]
 
     class Meta:
         verbose_name = "Enrollment"
@@ -58,11 +77,17 @@ class Enrollment(CustomModel):
         errors = ResponseDetails()
         errors.clear()
         
-        if not self.student.group:
+        student_group = self.student.group
+        
+        if student_group is None:
             errors.add_field_message('student', "Student currently is not in any group")
         
-        if self.subject.grade != self.student.group:
-            errors.add_field_message('subject', "Subject's grade is different from student's group grade")
+        if student_group:
+            if self.course.group != student_group:
+                errors.add_field_message('course', "Course group is different from student group")
+            
+            if self.subject.grade != student_group.grade:
+                errors.add_field_message('subject', "Subject's grade is different from student's group grade")
         
         if self.course.subject != self.subject:
             errors.add_field_message('course', "Course subject is different from enrollment subject")
@@ -70,8 +95,5 @@ class Enrollment(CustomModel):
         if self.course.year != self.year:
             errors.add_field_message('course', "Course year is different from enrollment year")
             
-        if self.course.subject.grade != self.subject.grade:
-            errors.add_field_message('course', "Course subject grade is different from enrollment subject grade")
-        
         if errors:
             raise ValidationError(errors.map)

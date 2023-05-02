ef0caf8b7d0d08e2b1c393bd87b52c8c7c8fd9ef
Limkaa <khamraevalim@gmail.com>
2023-05-02 18:53:31 +0600

    Enrollments serializers updated

--- backend/src/apps/core/modules/enrollments/serializers/editable.py
+++ backend/src/apps/core/modules/enrollments/serializers/editable.py
@@ -4,9 +4,15 @@ from .nested import EnrollmentNestedSerializer
 
 class EnrollmentModelCreateSerializer(EnrollmentModelSerializer):
     class Meta(EnrollmentModelSerializer.Meta):
-        read_only_fields = ['id', 'subject', 'year', 'created_at']
+        read_only_fields = ['id', 'subject', 'year', 'course', 'created_at']
+    
+    def to_representation(self, instance):
+        return EnrollmentNestedSerializer(instance).data
 
 
 class EnrollmentModelUpdateSerializer(EnrollmentModelSerializer):
+    class Meta(EnrollmentModelSerializer.Meta):
+        read_only_fields = ['id', 'student', 'subject', 'year', 'course', 'created_at']
+        
     def to_representation(self, instance):
         return EnrollmentNestedSerializer(instance).data

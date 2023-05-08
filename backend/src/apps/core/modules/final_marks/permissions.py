644b05de274dd589f54465d18bb7011fea026321
Limkaa <khamraevalim@gmail.com>
2023-05-08 12:55:57 +0600

    Added extended permissions for TermMark

---
+++ backend/src/apps/core/modules/final_marks/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework import permissions
+from ..permissions import *
+
+
+class IsMarkOfOwnSchool(OnlyOwnSchool):
+    def has_object_permission(self, request, view, obj):
+        return super().has_object_permission(request, view, obj.enrollment.course)
+
+class TermIsOpenedToFinalMarks(permissions.BasePermission):
+    message = 'Term is closed to evalutaion'
+    
+    def has_object_permission(self, request, view, obj):
+        return obj.term.is_opened_to_final_marks
+
+
+class IsTeacherOfEnrollmentCourse(IsCourseTeacher):
+    def has_object_permission(self, request, view, obj):
+        return super().has_object_permission(request, view, obj.enrollment.course)
+
+
+class IsTeacherOfEnrollmentStudentGroup(IsGroupTeacher):
+    def has_object_permission(self, request, view, obj):
+        return super().has_object_permission(request, view, obj.enrollment.student)
+
+
+class IsRelatedStudent(IsUserItself):
+    def has_object_permission(self, request, view, obj):
+        return super().has_object_permission(request, view, obj.enrollment.student)

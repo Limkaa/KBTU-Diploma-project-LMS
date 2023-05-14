f55b8350a00852865d280c0d4a294c6ac8b2ea00
Limkaa <khamraevalim@gmail.com>
2023-05-14 12:49:09 +0600

    Award rating signals added

--- backend/src/apps/core/modules/awards/apps.py
+++ backend/src/apps/core/modules/awards/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AwardsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.core.modules.awards"
+    
+    def ready(self) -> None:
+        from . import signals

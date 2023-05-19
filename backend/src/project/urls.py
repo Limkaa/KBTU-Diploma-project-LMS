b14c155309e27cf33552cee8ed4f540a6e07b3bd
Limkaa <khamraevalim@gmail.com>
2023-05-19 17:34:06 +0600

    Images path fix

--- backend/src/project/urls.py
+++ backend/src/project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(("apps.api.urls", "api"))),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
